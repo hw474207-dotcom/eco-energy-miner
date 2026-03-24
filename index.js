41d4dc8c5ab8dc2556d7cb89ee42b5b5ddd639e3
hw474207-dotcom <hw474207@gmail.com>
2026-03-24 21:47:02 +0500

    Refactor server to use environment variable for port

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Eco Energy Miner App is Live 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
